e223e7d9974062b3c93f5a5c2b7a94714a41a7a7
jykim3097 <jykim3097@gmail.com>
2022-03-07 23:45:43 +0900

    220307 푸는 중..

--- src/highscorekit/stackQueue/stackQueue3_다리를지나는트럭/Solution.java
+++ src/highscorekit/stackQueue/stackQueue3_다리를지나는트럭/Solution.java
@@ -1,9 +1,7 @@
 package highscorekit.stackQueue.stackQueue3_다리를지나는트럭;
 
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 
 public class Solution {
 
@@ -13,62 +11,63 @@ public class Solution {
 		
 		Queue<Integer> queue = new LinkedList<>();
 		Queue<Integer> cross = new LinkedList<>();
-		//Stack<Integer> time = new Stack<Integer>();
 		
 		for(int i=0; i<truck_weights.length; i++) {
 			queue.add(truck_weights[i]);
 		}System.out.println(queue);
-		
-		int t = 0;
+
+		int time = 0;
 		int sum = 0;
 		int n = 0;
-		
 		cross.add(0);
+		
 		while(!cross.isEmpty()) {
-			t++;
+			time++;
 			
 			if(cross.size() == bridge_length) {
 				sum -= cross.peek();
 				cross.poll();
 			}
 			
-			if(!queue.isEmpty()) {
+			if(queue.isEmpty() && cross.size() != bridge_length) {
+				cross.add(0);
+			}
+			
+			if(!queue.isEmpty()) {				
 				n = queue.peek();
 				sum += n;
 			} else {
-				cross.poll();
-				continue;
+				n = 0;
+				sum += n;
 			}
 			
 			if(sum > weight) {
-				if(cross.size() != bridge_length) {
-					t++;
-					cross.add(0);
-					sum -= n;
-				}
-			} else if(!queue.isEmpty()) {
+				sum -= queue.peek();
+				cross.add(0);
+			} else {
 				cross.add(n);
 				queue.poll();
-			} 
-			System.out.println(t+"s: "+cross);
+			}
+			
+			System.out.println(time);
 		}
-		answer = t - 1;
+		
 		System.out.println("answer: "+answer);		
 		return answer;
 	}
 	
 	public static void main(String[] args) {
-//		int bridge_length = 2;
-//		int weight = 10;
-//		int[] truck_weights = {7,4,5,6};
+		int bridge_length = 2;
+		int weight = 10;
+		int[] truck_weights = {7,4,5,6};
 		
 //		int bridge_length = 3;
 //		int weight = 15;
 //		int[] truck_weights = {9, 4, 1, 7, 9};
 		
-		int bridge_length = 100;
-		int weight = 100;
-		int[] truck_weights = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+//		int bridge_length = 100;
+//		int weight = 100;
+//		int[] truck_weights = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
 		
 //		int bridge_length = 100;
 //		int weight = 100;
