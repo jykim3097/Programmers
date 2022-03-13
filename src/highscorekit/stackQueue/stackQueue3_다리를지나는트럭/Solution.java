67d0bd154ec743704b15359ded840c4e27bba174
jykim3097 <jykim3097@gmail.com>
2022-03-13 23:06:47 +0900

    210313 문제 풀이 (100점)

--- src/highscorekit/stackQueue/stackQueue3_다리를지나는트럭/Solution.java
+++ src/highscorekit/stackQueue/stackQueue3_다리를지나는트럭/Solution.java
@@ -1,6 +1,5 @@
 package highscorekit.stackQueue.stackQueue3_다리를지나는트럭;
 
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -20,15 +19,16 @@ public class Solution {
 		int time = 0;
 		int sum = 0;
 		int n = 0;
+		boolean flag = false;
+		cross.add(0);
 
-		// 다리 길이가 트럭 개수보다 긴 경우 - 예외처리 필요함
-
-		//  다리에 트럭이 있는 경우 || 대기 트럭이 있는 경우
-		while(!cross.isEmpty() || !queue.isEmpty()) {
+		//  다리에 트럭이 있는 경우
+		while(!cross.isEmpty()) {
 			time++;
 			
-			// 다리에 트럭이 가득 찬 경우  || 대기 트럭이 빈 경우
-			if(cross.size() == bridge_length || queue.isEmpty()) {
+			// 다리에 트럭이 가득 찬 경우
+			if(cross.size() == bridge_length || flag || time == bridge_length) {
+				if(!flag) flag = true;
 				sum -= cross.peek();
 				cross.poll();
 			}
@@ -45,9 +45,11 @@ public class Solution {
 					cross.add(n);
 					queue.poll();
 				}
+			} else if(!flag) {
+				// 대기트럭이 없는데 가야할 길이 많이 남은 경우(다리가 꽉 차보지도 못하고 대기트럭이 없는 경우)
+				continue;
 			}
 		}
-		
 		answer = time;	
 		return answer;
 	}
@@ -58,17 +60,17 @@ public class Solution {
 //		int weight = 10;
 //		int[] truck_weights = {7,4,5,6};
 		
-		int bridge_length = 3;
-		int weight = 15;
-		int[] truck_weights = {9, 4, 1, 7, 9};
+//		int bridge_length = 3;
+//		int weight = 15;
+//		int[] truck_weights = {9, 4, 1, 7, 9};
 		
 //		int bridge_length = 100;
 //		int weight = 100;
 //		int[] truck_weights = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
 		
-//		int bridge_length = 100;
-//		int weight = 100;
-//		int[] truck_weights = {10};
+		int bridge_length = 100;
+		int weight = 100;
+		int[] truck_weights = {10};
 		
 		solution(bridge_length, weight, truck_weights);
 	}
